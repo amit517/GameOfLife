2faa5a3f2cab0cbe7feca3a3f099a6755f291bf2
Amit Kundu <kundu.amit517@gmail.com>
2022-05-29 22:05:23 +0600

    VM updated

--- app/src/main/java/com/assesment/gameoflife/GameActivityVM.kt
+++ app/src/main/java/com/assesment/gameoflife/GameActivityVM.kt
@@ -4,19 +4,13 @@ import androidx.lifecycle.viewModelScope
 import com.assesment.base.viewmodel.BaseViewModel
 import com.assesment.gameoflife.gamehelper.GameOfLife
 import com.assesment.gameoflife.gamehelper.GameView
-import com.assesment.gameoflife.gamehelper.GameWorld
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class GameActivityVM @Inject constructor(gameWorld: GameWorld) : BaseViewModel() {
-    val game: GameOfLife
-
-    init {
-        game = GameOfLife(gameWorld)
-    }
+class GameActivityVM @Inject constructor(val game: GameOfLife) : BaseViewModel() {
 
     fun runGame(gameView: GameView) {
         viewModelScope.launch(Dispatchers.Main) {
